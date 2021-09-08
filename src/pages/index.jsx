c0f116da4723029cbbf4c67789fb122eacc0c4c3
Elton Brayner <eltonbrayner@iMac-de-Elton.local>
2021-09-07 21:02:11 -0300

    fix cross line

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -77,6 +77,7 @@ export default function Home() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
           integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
       </Head>
